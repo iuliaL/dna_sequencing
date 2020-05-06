97dafd42594cdd81253cdc1d30a1ba7eef1cb5e1
iulia <iuliamarialungu@yahoo.com>
2020-05-06 13:16:58 +0200

    preprocessing before matching

--- BoyerMooreMatching.py
+++ BoyerMooreMatching.py
@@ -1,12 +1,9 @@
-from lib.BoyerMoorePreprocessing import BoyerMoore as bm
-
 # GCTAGCTC
 # TCAA
 
-def BoyerMooreMatching(pattern, genome):
+def BoyerMooreMatching(pattern, boyer_moore_obj, genome):
     '''Find all the positions of a given pattern in genome using BoyerMoore'''
 
-    boyerMoore = bm(pattern)
     positions = []
     i = 0
     k = len(pattern)
@@ -19,13 +16,13 @@ def BoyerMooreMatching(pattern, genome):
             if not pattern[j] == current_kmer[j]:
                 mismatched = True
                 mismatched_char = current_kmer[j]
-                skip_bad_char = boyerMoore.bad_character_rule(j, mismatched_char)
-                skip_good_suffix = boyerMoore.good_suffix_rule(j)
+                skip_bad_char = boyer_moore_obj.bad_character_rule(j, mismatched_char)
+                skip_good_suffix = boyer_moore_obj.good_suffix_rule(j)
                 shift = max(shift, skip_bad_char, skip_good_suffix)
                 break
         if not mismatched:
             positions.append(i)
-            shift = max(shift, boyerMoore.match_skip()) # pattern matches entirely the current kmer
+            shift = max(shift, boyer_moore_obj.match_skip()) # pattern matches entirely the current kmer
         i += shift
 
     return positions 
